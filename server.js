599591a256ff6b4d72e52821f4f467d84c1ef6a6
Amna Zletni <azletni13@gmail.com>
2016-10-20 15:32:51 -0400

    file restructure

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 'use strict';
 
-require('dotenv').config();
+// require('dotenv').config();
 
-const ENV         = process.env.ENV || "development";
+// const ENV         = process.env.ENV || "development";
 const pg          = require('pg');
 const express     = require('express');
 const PORT        = 3000;
 const app         = express();
-const knexConfig  = require('.knexfile');
-const knex        = require('knex')(knexConfig[ENV]);
+// const knexConfig  = require('.knexfile');
+// const knex        = require('knex')(knexConfig[ENV]);
 const userRoutes  = require('./routes/users.js');
 const homeRoutes  = require('./routes/home.js');
 const goalRoutes  = require('./routes/goal.js');
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/../public'));
-app.set('views', __dirname + '/../views');
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
 
 
 app.use('/login', userRoutes());
