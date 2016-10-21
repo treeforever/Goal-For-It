82e33cd11a832f7c7585cc7e5f850acef74db893
Amna Zletni <azletni13@gmail.com>
2016-10-21 09:18:41 -0400

    cleaning up unused files

--- src/App.js
+++ src/App.js
@@ -9,14 +9,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Sidebar />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>GOAL FOR IT!</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <nav>
           <Sidemenu />
         </nav>
