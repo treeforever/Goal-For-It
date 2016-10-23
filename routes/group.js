2a4614b80009d4eddea992a1400001b90a561343
Amna Zletni <azletni13@gmail.com>
2016-10-23 12:29:48 -0400

    adds untracked files

---
+++ routes/group.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+
+
+module.exports = (knex) => {
+
+  //performs get request to database for goal informations
+  router.get('/', (req, res) => {
+    knex.select('*')
+      .from('users')
+      .innerJoin('users_groups', 'users_groups.user_id', 'users.user_id')
+      .innerJoin('groups', 'groups.group_id', 'users_groups.group_id')
+      .where('groups.group_id', 1)
+      .select('groups.name', 'groups.description', 'users.username')
+      .then((results) => {
+        console.log(results)
+        res.json(results);
+      })
+  });
+
+  return router;
+};
